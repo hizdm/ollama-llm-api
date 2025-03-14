ed7e8ea415dd1e600c5d868e870bf4203ebf27bb
hizdm <zhangzhili@outlook.com>
2025-03-14 15:37:42 +0800

    Update generate.py

--- controller/generate.py
+++ controller/generate.py
@@ -8,6 +8,7 @@ from ollama import Client
 from library.util import util
 from controller.base import BaseHandler
 from controller.prompt import PromptHandler
+from controller.strategy import StrategyHandler
 
 """
 Ollama Generate
@@ -19,8 +20,9 @@ class GenerateHandler(BaseHandler):
 			model   = data.get('model', None)        # 模型名称
 			role    = data.get('role', 'user')       # 角色名称
 			content = data.get('content', 'Hello')   # 对话内容[输入内容(默认Hello)->提示工程处理]
-			pid     = data.get('pid', None)          # 项目标识
 			prompt  = data.get('prompt', None)       # 提示工程标识
+			is_stream   = data.get('is_stream', 0)   # 是否流式输出
+			temperature = data.get('temperature', float(util.fetch_conf('global.ini', 'generate', 'temperature')))   # 温度
 		except json.JSONDecodeError:
 			self.set_status(400)
 			self.write({'code':400201, 'message':'invalid json', 'data':[]})
@@ -33,10 +35,6 @@ class GenerateHandler(BaseHandler):
 		}
 
 		# Params
-		if pid is None:
-			output = {'code':400202, 'message':'pid is null', 'data':[]}
-			self.write(json.dumps(output))
-			return
 		if model is None:
 			output = {'code':400203, 'message':'model is null', 'data':[]}
 			self.write(json.dumps(output))
@@ -56,6 +54,15 @@ class GenerateHandler(BaseHandler):
 				uid = payload.get('uid')
 
 		# Strategy
+		objStrategy = StrategyHandler(uid)
+		s1 = objStrategy.sInterval({})
+		if s1['code'] != 0:
+			self.write(json.dumps(s1))
+			return
+		s2 = objStrategy.sTimetotal({})
+		if s2['code'] != 0:
+			self.write(json.dumps(s2))
+			return
 
 		# Prompt
 		if prompt is not None:
@@ -69,8 +76,25 @@ class GenerateHandler(BaseHandler):
 		# Generate
 		try:
 			client = Client(host=util.fetch_conf('global.ini', 'llm', 'host'))
-			response = client.generate(model=model, prompt=content)
-			output["data"] = response['response']
+			#response = client.generate(model=model, prompt=content)
+			#output["data"] = response['response']
+			response = client.generate(model=model, 
+				prompt=content,
+				stream=False,
+				options={
+				    "temperature": temperature, 
+					"num_keep": int(util.fetch_conf('global.ini', 'generate', 'num_keep')),
+					"num_ctx": int(util.fetch_conf('global.ini', 'generate', 'num_ctx')),
+				})
+			#output["data"] = response['response']
+			if is_stream == 1:
+				for chunk in response:
+					self.write(chunk['response'])
+					self.flush()
+			else:
+				output["data"] = response.message.content
+				self.write(json.dumps(output, ensure_ascii=False))
+
 		except Exception as e:
 			output = {
 				"code": 400444,
@@ -78,4 +102,4 @@ class GenerateHandler(BaseHandler):
 				"data": {}
 			}
 
-		self.write(json.dumps(output))
+			self.write(json.dumps(output, ensure_ascii=False))
